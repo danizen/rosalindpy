5c1bbb3c1a1fc5220b516754d60855f917a5a0ce
Dan Davis <daniel.davis@nih.gov>
2017-01-18 13:19:45 -0500

    Believe the code is now correct.

--- iprb/iprb.py
+++ iprb/iprb.py
@@ -8,11 +8,14 @@ class Allele(object):
     HETEROZYGOUS = 1
     HOMOZYGOUS_RECESSIVE = 2
 
-
     @classmethod
     def all(cls):
-        return [cls.HOMOZYGOUS, cls.HETEROZYGOUS, cls.HOMOZYGOUS_RECESSIZE]
-        
+        return [cls.HOMOZYGOUS, cls.HETEROZYGOUS, cls.HOMOZYGOUS_RECESSIVE]
+
+    @classmethod
+    def hasfeature(cls, allele_type):
+        return False if allele_type == cls.HOMOZYGOUS_RECESSIVE else True
+
 
 class Population(object):
 
@@ -32,64 +35,85 @@ class Population(object):
         return self.counts[organism_type]/self.total
 
     def take(self, organism_type):
-        assert self.counts[organism_type] > 0
         newcounts = self.counts.copy()
-        newcounts[organism_type] -= 1
+        if self.counts[organism_type] > 0:
+            newcounts[organism_type] -= 1
         return Population(newcounts)
 
 
-class ProbalityTreePath(object):
+# Hard to fix - these are business rules.   We can improve by accumulating probability
+# of child alleles to some matrix.
 
-    def __init__(self, population, probality, allele_mother, allele_father)
-        self.probality = probality
-        self.allele_mother = allele_mother
-        self.allele_father = allele_father
+def accumulate_child_probability(children, probability, allele_mother, allele_father):
 
-    def hasfeature(self):
-        return "F" in self.path
-
-
-def add_tree_paths(paths, allele_mother, allele_father, probability):
-    switch 
-
-# we assume that all organisms are hermaphrodites
-
-# Let Random Variable X := 1st organism's allele
-# Let Random Variable Y : 2nd organism's allele
-# Let Random Variable Z : offspring's allele
-# Let the factor be written D for dominant or d for recessive
-
-# Pr{Z has factor} = Pr{Z == homozygous} + Pr{Z == heterozygous}
-# Pr{Z has factor} = 1 - Pr{Z == homozygous recessive}
-
-# Pr{Z == homozygous recessive} 
-#                     = Pr{X == homozygous recessive}Pr{Y == homozygous recessive}
-#                     + (Pr{X == heterozygous}/2)Pr{Y == homozygous recessive}
-#                     + Pr{X == homozygous recessive}(Pr{Y == heterozygous}/2)
-#                     + (Pr{X == heterozygous}Pr{Y == heterozygous}/4)
-
-# Pr{X == homozygous} = k/(k+m+n) 
-# Pr{X == heterozygous} = m/(k+m+n)
-# Pr{X == homozygous recessive} = n/(k+m+n)
-
-# Pr{Y == homozyous}/Pr{X == homozygous} = (k-1)/(k+m+n-1)
-# Pr{Y == homozyous}/Pr{X == hetezygous} = k/(k+m+n-1)
-# Pr{Y == homozyous)/Pr{X == homozygous recessive} = k/(k+m+n-1)
-
-# Pr{Y == heterozygous}/Pr{X == homozygous} = m/(k+m+n-1)
-# Pr{Y == heterozygous}/Pr{X == heterozygous} = (m-1)/(k+m+n-1)
-# Pr{Y == heterozygous}/Pr{X == homozygous recessive} = m/(k+m+n-1)
-
-# Pr{Y == homozygous recessive}/Pr{X == homozygous} = n/(k+m+n-1)
-# Pr{Y == homozygous recessive}/Pr{X == heterozygous} = n/(k+m+n-1)
-# Pr{Y == homozygous recessive}/Pr{X == homozygous recessive} = (n-1)/(k+m+n-1)
+    if allele_mother == Allele.HOMOZYGOUS:
+        if allele_father == Allele.HOMOZYGOUS:
+            children[ Allele.HOMOZYGOUS ] += probability
+        elif allele_father == Allele.HOMOZYGOUS_RECESSIVE:
+            children[ Allele.HETEROZYGOUS ] += probability
+        else:
+            children[ Allele.HOMOZYGOUS ] += probability / 2
+            children[ Allele.HETEROZYGOUS ] += probability / 2
+    elif allele_mother == Allele.HOMOZYGOUS_RECESSIVE:
+        if allele_father == Allele.HOMOZYGOUS:
+            children[ Allele.HETEROZYGOUS ] += probability
+        elif allele_father == Allele.HOMOZYGOUS_RECESSIVE:
+            children[ Allele.HOMOZYGOUS_RECESSIVE ] += probability
+        else:
+            children[ Allele.HETEROZYGOUS ] += probability / 2
+            children[ Allele.HOMOZYGOUS_RECESSIVE ] += probability / 2
+    else:
+        if allele_father == Allele.HOMOZYGOUS:
+            children[ Allele.HOMOZYGOUS ] += probability / 2
+            children[ Allele.HETEROZYGOUS ] += probability / 2
+        elif allele_father == Allele.HOMOZYGOUS_RECESSIVE:
+            children[ Allele.HETEROZYGOUS ] += probability / 2
+            children[ Allele.HOMOZYGOUS_RECESSIVE ] += probability / 2
+        else:
+            # both parents are heterozygous
+            children[ Allele.HOMOZYGOUS ] += probability / 4
+            children[ Allele.HOMOZYGOUS_RECESSIVE ] += probability / 4
+            children[ Allele.HETEROZYGOUS ] += probability / 2
 
 
 
+def guts(k, m, n, verbose=True):
 
-# Pr{Y == heteroy
+    # k organisms are homozygous for a factor (both chromosomes dominant)
+    # m organisms are heterozygous for a factor (chromosomes different)
+    # n oragnisms are homozygous recessize for a factor (both chromosomes recessive)
+    # we assume that all organisms are hermaphrodites
 
-# Pr{1st organim
+    if verbose:
+        print('k = %d, m = %d, n = %d' % (k, m, n)) 
+
+
+    # chances of children of each type
+    children = np.zeros(3, dtype=np.float64)
+
+    # population counts, and calculation of probability of member of each type
+    population = Population([k, m, n])
+
+    # calculate chances of each outcome (child allele) in decision tree
+    for allele_mother in Allele.all():
+        probality_mother = population.probability(allele_mother)
+        subpopulation = population.take(allele_mother)
+
+        for allele_father in Allele.all():
+
+            probability_father = subpopulation.probability(allele_father)
+            probability = probality_mother * probability_father
+
+            accumulate_child_probability(children, probability, allele_mother, allele_father)
+
+    if verbose:
+        print("chance child is homozygous = %lf" % children[Allele.HOMOZYGOUS])
+        print("chance child is heterozygous = %lf" % children[Allele.HETEROZYGOUS])
+        print("chance child is homozygous recessive = %lf" % children[Allele.HOMOZYGOUS_RECESSIVE])
+
+
+    return children[Allele.HOMOZYGOUS] + children[Allele.HETEROZYGOUS]
+
 
 def main():
     parser = argparse.ArgumentParser(prog=basename(__file__), description='Fubar')
@@ -100,32 +124,8 @@ def main():
 
     opts = parser.parse_args()
 
-    # k organisms are homozygous for a factor (both chromosomes dominant)
-    # m organisms are heterozygous for a factor (chromosomes different)
-    # n oragnisms are homozygous recessize for a factor (both chromosomes recessive)
-
-    if opts.verbose:
-        print('k = %d, m = %d, n = %d' % (opts.k, opts.m, opts.n)) 
-
-
-    paths = []
-
-    population = Popuplation([opts.k, opts.m, opts.n])
-    for allele_mother in Allele.all():
-        probality_mother = population.probability(allele_mother)
-        subpopulation = population.take(allele_mother)
-
-        for allele_father in Allele.all():
-
-            probability_father = subpopulation.probability(allele_father)
-            probability = probality_mother * probability_father
-
-            add_tree_paths(path, allele_mother, probability_mother, allele_father, probability_father)
-
-    if opts.verbose:
-        for path in paths:
-            print("path %s 
-
+    chance_has_feature = guts(opts.k, opts.m, opts.n, opts.verbose)
+    print("%lf" % chance_has_feature)
 
 
 if __name__ == '__main__':
