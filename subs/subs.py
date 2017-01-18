6cf29a673b380c67fe544e2959f9eb9082ffd3b5
Dan Davis <daniel.davis@nih.gov>
2017-01-18 13:30:55 -0500

    Finished subs problem by just using Knuth-Morris-Pratt rather than re-implementing.
    - Faster algorithms available since the alphabet is restricted

--- subs/subs.py
+++ subs/subs.py
@@ -1,9 +1,11 @@
 import argparse
 from os.path import basename, dirname, join
 import io
+import re
 
 parser = argparse.ArgumentParser(prog=basename(__file__), description='Fubar')
 parser.add_argument('datafile')
+parser.add_argument('--verbose', '-v')
 
 opts = parser.parse_args()
 
@@ -11,7 +13,14 @@ f = open(opts.datafile, 'r')
 haystack = f.readline().strip()
 needle = f.readline().strip()
 
-print('haystack = %s' % haystack)
-print('needle = %s' % needle)
+if opts.verbose:
+    print('haystack = %s' % haystack)
+    print('needle = %s' % needle)
 
 # use Python primitives a cop out?
+
+i = haystack.find(needle)
+while i > -1:
+    print(i+1)
+    i = haystack.find(needle, i+1)
+
