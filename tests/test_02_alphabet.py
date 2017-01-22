a793baad961ec8225590433a09a8d63e8f6a3e6c
Daniel Davis <dan@danizen.net>
2017-01-21 23:56:38 -0500

    Test RNA alphabet

--- tests/test_02_alphabet.py
+++ tests/test_02_alphabet.py
@@ -35,3 +35,24 @@ def test_rna_isvalid():
 
 def test_rna_isvalid_notstrictk():
     assert RNA.isvalid('AUCC-AGCU', strict=False)
+
+def test_rna_not_isvalid():
+    assert not RNA.isvalid('ATCCAGCT')
+
+def test_rna_not_isvalid_notstrict():
+    assert not RNA.isvalid('AUCC-AGCU*', strict=False)
+
+def test_rna_letter2pos():
+    assert RNA.letter2pos('A') == 0
+    assert RNA.letter2pos('C') == 1
+    assert RNA.letter2pos('G') == 2
+    assert RNA.letter2pos('U') == 3
+
+def test_rna_pos2letter():
+    assert RNA.pos2letter(0) == 'A'
+    assert RNA.pos2letter(1) == 'C'
+    assert RNA.pos2letter(2) == 'G'
+    assert RNA.pos2letter(3) == 'U'
+
+def test_rna_len():
+    assert len(RNA) == 4
