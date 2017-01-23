e9b7507e7d75e50f977ad30e8d4e39959740de7f
Dan Davis <daniel.davis@nih.gov>
2017-01-23 16:53:53 -0500

    Implement it also with biopython.

--- orf/orf.py
+++ orf/orf.py
@@ -8,6 +8,8 @@ from rosalindpy import translate
 from rosalindpy import fasta
 from rosalindpy import utils
 
+from Bio import SeqIO
+
 
 def guts(datafile):
     sequences = fasta.readsimple(datafile)
@@ -22,17 +24,52 @@ def guts(datafile):
     return frames
 
 
+def guts_biopython(datafile):
+    seqrec = SeqIO.read(datafile, 'fasta')
+    seq = seqrec.seq
+
+    nbp = len(seq)
+
+    frames = {}
+    pos = seq.find('ATG')
+    while (pos > 0):
+        framlen = nbp - pos 
+        framlen -= framlen % 3
+        subseq = seq[pos:framlen]
+        prot = subseq.translate(to_stop=True)
+        frames[pos] = prot
+        pos = seq.find('ATG', pos+1)
+
+    revseq = seq.reverse_complement()
+    pos = revseq.find('ATG')
+    while (pos > 0):
+        framlen = nbp - pos 
+        framlen -= framlen % 3
+        subseq = revseq[pos:framlen]
+        prot = subseq.translate(to_stop=True)
+        frames[-1 * pos] = prot
+        pos = revseq.find('ATG', pos+1)
+
+    return frames
+
+
 def main():
     parser = argparse.ArgumentParser(prog=basename(__file__), 
                                      description='Open Reading Frames')
-    parser.add_argument('datafile')
-    parser.add_argument('--verbose', '-v', metavar='LEVEL', type=int)
+    parser.add_argument('datafile', help='In fasta format')
+    parser.add_argument('--verbose', '-v', metavar='LEVEL', type=int,
+                        help='Configure logging to this level of output')
+    parser.add_argument('--biopython', default=False, action='store_true',
+                        help='Use biopython to check our logic')
     opts = parser.parse_args()
 
     if opts.verbose:
         logging.basicConfig(level=opts.verbose)
 
-    frames = guts(opts.datafile)
+    if opts.biopython:
+        frames = guts_biopython(opts.datafile)
+    else:
+        frames = guts(opts.datafile)
     if opts.verbose:
         for k, v in frames.items():
             print('at %d: %s' % (k, v))
