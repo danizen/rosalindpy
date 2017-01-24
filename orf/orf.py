0d78c64bdc17931709a13fa8ebe55c5c830e74d6
Daniel Davis <dan@danizen.net>
2017-01-23 23:42:59 -0500

    Still struggling with orf.

--- orf/orf.py
+++ orf/orf.py
@@ -7,6 +7,7 @@ import logging
 from rosalindpy import translate
 from rosalindpy import fasta
 from rosalindpy import utils
+from rosalindpy import alphabet
 
 from Bio import SeqIO
 from Bio.Alphabet import IUPAC
@@ -19,13 +20,10 @@ def guts(datafile):
     sequences = fasta.readsimple(datafile)
 
     assert len(sequences) == 1
-    utils.validate_dna(sequences)
+    k = next(iter(sequences.keys()))
+    seq = sequences[k]
 
-    frames = None
-    for k, v in sequences.items():
-        frames = translate.open_reading_frames(v)
-
-    return frames
+    return translate.open_reading_frames(seq)
 
 
 def guts_biopython(datafile):
@@ -65,14 +63,14 @@ def main():
     parser = argparse.ArgumentParser(prog=basename(__file__), 
                                      description='Open Reading Frames')
     parser.add_argument('datafile', help='In fasta format')
-    parser.add_argument('--verbose', '-v', metavar='LEVEL', type=int,
-                        help='Configure logging to this level of output')
+    parser.add_argument('--verbose', '-v', default=False, action='store_true',
+                        help='Verbose output')
     parser.add_argument('--biopython', default=False, action='store_true',
                         help='Use biopython to check our logic')
     opts = parser.parse_args()
 
     if opts.verbose:
-        logging.basicConfig(level=opts.verbose)
+        logging.basicConfig(level=1)
 
     if opts.biopython:
         frames = guts_biopython(opts.datafile)
