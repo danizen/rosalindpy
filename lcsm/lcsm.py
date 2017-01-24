c80976aa15322173e6178d12747f9ceeb5f5ec15
Daniel Davis <dan@danizen.net>
2017-01-24 00:43:41 -0500

    Blah.

--- lcsm/lcsm.py
+++ lcsm/lcsm.py
@@ -20,11 +20,35 @@ def guts(sequences):
     #      the two.
     #    - Take a longest node that has no kids (is a leaf)
 
-    # The hope that this would be O(n) on characters is fleeting.
-    # we have to be able to color the nodes.  
+    Process first sequence into suffix tree
 
-    # Maybe we swap graphs. We always build a new graph, copying from the old graph.
-    # Anything we cannot copy, we drop after the next sequence.
+    
+    Loop invariant:
+        tree := sequences present in all sequences so far processed
+
+    For each subsequent sequence, 
+        do the stuff
+        
+    tree = tree of sequences in all 
+
+    # The hope that this would be O(n) on characters is fleeting.  we have to
+    # be able to color the nodes.  
+
+    # Maybe we swap graphs. We always build a new graph, copying from the old
+    # graph.  Anything we cannot copy, we drop after the next sequence.
+
+    # NOTE: The design above is a classic algorithm based on suffix trees, it
+    # is almost equivalent to say that the edges are subsequent characters.
+    # This permits more than one character at a time. There is also a dynamic
+    # programming solution, but I don't get it yet.
+
+    # There are python modules that implement this already -
+    # https://github.com/ptrus/suffix-trees for example.  Worthwhile to program
+    # it myself?   Biopython probably has this somewhere, too.
+
+    # I don't see how the DP solution will generalize to N such strings, but I
+    # do see how the suffix tree version will do so.
+
     return 'NYI'
 
 
